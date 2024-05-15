a210e9699a178bb9c67845d35360da81c61ff67f
BriRoz1 <loko2546mail.com>
2024-05-15 13:13:49 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,208 @@
+// server.js
+const express = require('express');
+const mssql = require('mssql');
+const cors = require('cors'); // Importa el paquete cors
+const bodyParser = require('body-parser'); // Importa body-parser
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// Configuración de la conexión a la base de datos
+const dbConfig = {
+  user: 'CloudSA365db3f2',
+  password: 'Andresrozo3',
+  server: 'proyectoudistrital.database.windows.net',
+  database: 'preferencias',
+  options: {
+    encrypt: true, // Habilita el cifrado
+    trustServerCertificate: false // Deshabilita la confianza en el certificado del servidor
+  }
+};
+
+// Conectar a la base de datos y crear la tabla si no existe
+async function connectToDatabase() {
+  try {
+    await mssql.connect(dbConfig);
+    console.log('Connected to the database successfully!');
+
+    // Verificar si la tabla existe
+    const tableExists = await mssql.query(`SELECT OBJECT_ID('listadistribucion', 'U') AS table_id`);
+    if (!tableExists.recordset[0].table_id) {
+      // La tabla no existe, así que la creamos
+      await mssql.query(`
+        CREATE TABLE listadistribucion (
+          id INT IDENTITY(1,1) PRIMARY KEY,
+          name NVARCHAR(100),
+          
+        )
+      `);
+      console.log('Table "preferencias" created successfully!');
+    }
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+}
+
+connectToDatabase();
+
+// Definir rutas para la API REST
+app.get('/api/preferencias', async (req, res) => {
+  try {
+    const result = await mssql.query('SELECT * FROM preferencias');
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// TRAE LAS LISTAS DE DISTRIBUCION PARA SER MOSTRADA EN LOS CHECKBOXES
+
+app.get('/api/listas', async (req, res) => {
+  try {
+    const result = await mssql.query('SELECT * FROM listadistribucion');
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// METODO PARA INSERTAR O ACTUALIZAR LAS PREFERENCIAS DE UN USUARIO
+
+app.post('/api/preferencias', async (req, res) => {
+  try {
+    const { idprofile, name, email, preferencias } = req.body;
+
+    // Verificar si ya existen preferencias para el idprofile actual
+    const existingRecord = await mssql.query(`
+      SELECT * FROM preferencias WHERE idprofile = '${idprofile}'
+    `);
+
+    if (existingRecord.recordset.length > 0) {
+      // Si ya existen preferencias, actualizamos en lugar de insertar
+      const result = await mssql.query(`
+        UPDATE preferencias 
+        SET name = '${name}', email = '${email}', preferencia = '${JSON.stringify(preferencias)}'
+        WHERE idprofile = '${idprofile}'
+      `);
+    } else {
+      // Si no existen preferencias, insertamos
+      const result = await mssql.query(`
+        INSERT INTO preferencias (idprofile, name, email, preferencia)
+        VALUES ('${idprofile}', '${name}', '${email}', '${JSON.stringify(preferencias)}')
+      `);
+    }
+
+    res.status(200).json({ message: 'Preferencias guardadas exitosamente' });
+  } catch (error) {
+    console.error('Error al guardar preferencias:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// VERIFICA SI EL ID YA ESTA REGISTRADO EN LA BD PARA ACTUALIZAR O CREAR NUEVOS REGISTROS.
+
+app.get('/api/preferencias/:idProfile/exists', async (req, res) => {
+  try {
+    const { idProfile } = req.params;
+    const result = await mssql.query(`SELECT TOP 1 * FROM preferencias WHERE idprofile = '${idProfile}'`);
+    res.json(result.recordset.length > 0);
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// TRAER LAS PREFERENCIAS DEL USUARIO ACTUALMENTE LOGUEADO
+
+app.get('/api/preferencias/:idProfile', async (req, res) => {
+  try {
+    const { idProfile } = req.params;
+    const result = await mssql.query(`SELECT * FROM preferencias WHERE idprofile = '${idProfile}'`);
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+//TRAE LOS EMAIL DE LOS USUARIOS COMPARANDO EL ID Y LA PREFERENCIA. 
+
+app.get('/api/preferencias/:preferencia/email', async (req, res) => {
+  try {
+    const { preferencia } = req.params;
+    // Dividir el parámetro de preferencia en un array
+    const preferencias = preferencia.split(',');
+    // Construir la consulta SQL con una cláusula WHERE que use LIKE
+    const query = `SELECT email FROM preferencias WHERE preferencia LIKE '%${preferencias[0]}%'`;
+    // Ejecutar la consulta SQL
+    const result = await mssql.query(query);
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Error querying the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/api/notification-permission', async (req, res) => {
+  try {
+    const { userId, permissionGranted } = req.body;
+
+    // Verificar si el usuario ya tiene un registro en la tabla de permisos
+    const existingRecord = await mssql.query(`
+      SELECT * FROM permisos WHERE userId = '${userId}'
+    `);
+
+    if (existingRecord.recordset.length > 0) {
+      // Si ya existe un registro para este usuario, actualizamos el permiso
+      await mssql.query(`
+        UPDATE permisos 
+        SET permissionGranted = ${permissionGranted ? 1 : 0}
+        WHERE userId = '${userId}'
+      `);
+    } else {
+      // Si no existe un registro para este usuario, lo insertamos
+      await mssql.query(`
+        INSERT INTO permisos (userId, permissionGranted)
+        VALUES ('${userId}', ${permissionGranted ? 1 : 0})
+      `);
+    }
+
+    res.status(200).json({ message: 'Permiso de notificación guardado correctamente' });
+  } catch (error) {
+    console.error('Error al guardar el permiso de notificación:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+app.get('/api/notification-permission/users', async (req, res) => {
+  try {
+    // Consultar la base de datos para obtener los usuarios con permisos de notificación
+    const result = await mssql.query(`
+      SELECT userId FROM permisos WHERE permissionGranted = 1
+    `);
+
+    // Obtener los userId de los registros devueltos
+    const users = result.recordset.map(record => record.userId);
+
+    // Devolver la lista de usuarios con permisos de notificación
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error obteniendo usuarios con permisos de notificación:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
